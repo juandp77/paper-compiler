c0af33b99f02feaa86d221edb31aed5916b37676
Juand <juandp77@gmail.com>
2019-05-07 09:31:25 +0200

    Update compile-manuscript.sh

--- compile-manuscript.sh
+++ compile-manuscript.sh
@@ -1,9 +1,14 @@
+
+###################################
+# Compile docs by juandp77 #
+###################################
+
 # Make figures
 #pandoc -H margins.sty list_of_figures.md -o list_of_figures.pdf
 # Make the manuscript
-pandoc -H ~/Dropbox/MyPapers/csl/margins.sty --bibliography  ~/Dropbox/MyPapers/library.bib --csl ~/Dropbox/MyPapers/csl/styles/american-medical-association.csl manuscript.md -o manuscript.pdf
+pandoc -H ~/Dropbox/MyPapers/csl/margins.sty --bibliography  ~/Dropbox/MyPapers/library.bib --csl ~/Dropbox/MyPapers/csl/styles/journal-of-neurosurgery.csl manuscript.md -o manuscript.pdf
 # Make Word file
-pandoc -H ~/Dropbox/MyPapers/csl/margins.sty --bibliography  ~/Dropbox/MyPapers/library.bib --csl ~/Dropbox/MyPapers/csl/styles/american-medical-association.csl manuscript.md -o manuscript.docx
+pandoc -H ~/Dropbox/MyPapers/csl/margins.sty --bibliography  ~/Dropbox/MyPapers/library.bib --csl ~/Dropbox/MyPapers/csl/styles/journal-of-neurosurgery.csl manuscript.md -o manuscript.docx
 pandoc -H ~/Dropbox/MyPapers/csl/margins.sty title.md -o title.docx
 # Combine the two using PDFtools (pdftools@skynet.be)
 #pdfcat git_manuscript.pdf list_of_figures.pdf > git_ms.pdf
